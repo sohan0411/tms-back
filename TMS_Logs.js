bb3096092ef7aa7acca2c9bc4172913b958fbe0f
Kaushal Pohekar <kaushalpohekar1@gmail.com>
2023-07-06 17:17:38 +0530

    THreshold

--- TMS_Logs.js
+++ TMS_Logs.js
@@ -1,33 +1,5 @@
 const db = require('./db');
 
-function getRandomNumber(min, max) {
-  return Math.random() * (max - min) + min;
-}
-
-function testData() {
-  for (let i = 1; i <= 9; i++) {
-    const DeviceUID = `SL0120230${i}`;
-    const Temperature = getRandomNumber(35, 50).toFixed(2);
-    const Humidity = getRandomNumber(40, 70).toFixed(2);
-    const Timestamp = new Date().toISOString();
-
-    const data = {
-      DeviceUID,
-      Temperature,
-      Humidity,
-      Timestamp
-    };
-
-    const entry = "INSERT INTO actual_data SET ?";
-    db.query(entry, data, (error, results) => {
-      if (error) {
-        console.log("Error while inserting data", error);
-      } else {
-        console.log("Data inserted successfully!");
-      }
-    });
-  }
-}
 
 function monitorDevice() {
   const selectTriggerQuery = 'SELECT tms_devices.DeviceUID, tms_trigger.TriggerValue FROM tms_trigger JOIN tms_devices ON tms_trigger.DeviceUID = tms_devices.DeviceUID';
@@ -94,6 +66,4 @@ function monitorDevice() {
 }
 
 
-
-setInterval(testData, 1000);
 setInterval(monitorDevice, 1000);
---
+++ test.js
@@ -0,0 +1,32 @@
+const db = require('./db');
+
+function getRandomNumber(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function testData() {
+  for (let i = 1; i <= 9; i++) {
+    const DeviceUID = `SL0120230${i}`;
+    const Temperature = getRandomNumber(35, 50).toFixed(2);
+    const Humidity = getRandomNumber(40, 70).toFixed(2);
+    const Timestamp = new Date().toISOString();
+
+    const data = {
+      DeviceUID,
+      Temperature,
+      Humidity,
+      Timestamp
+    };
+
+    const entry = "INSERT INTO actual_data SET ?";
+    db.query(entry, data, (error, results) => {
+      if (error) {
+        console.log("Error while inserting data", error);
+      } else {
+        console.log("Data inserted successfully!");
+      }
+    });
+  }
+}
+
+setInterval(testData, 1000);
