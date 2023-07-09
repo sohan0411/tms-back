dd184c0e1d7e8a976e02e63edc67114312aef8e3
Kaushal Pohekar <kaushalpohekar1@gmail.com>
2023-07-09 14:56:08 +0530

    Stop Data

--- TMS_Logs.js
+++ TMS_Logs.js
@@ -4,7 +4,7 @@ const db = require('./db');
 function monitorDevice() {
   const selectTriggerQuery = 'SELECT tms_devices.DeviceUID, tms_trigger.TriggerValue FROM tms_trigger JOIN tms_devices ON tms_trigger.DeviceUID = tms_devices.DeviceUID';
 
-  db.query(selectTriggerQuery, (error, triggerResults) => {
+  /*db.query(selectTriggerQuery, (error, triggerResults) => {
     if (error) {
       console.error('Error executing the select query: ', error);
       return;
@@ -61,7 +61,43 @@ function monitorDevice() {
         });
       }
     });
+  });*/
+  db.query(selectLatestDataQuery, deviceUIDs, (error, latestDataResults) => {
+  if (error) {
+    console.error('Error executing the latest data select query: ', error);
+    return;
+  }
+
+  const insertLogQuery = `INSERT INTO tms_trigger_logs (DeviceUID, Temperature, Humidity, TimeStamp, Status) VALUES ?`;
+  const insertLogValues = [];
+  const currentTimeStamp = new Date(); // Get the current timestamp
+
+  deviceData.forEach((device) => {
+    const latestData = latestDataResults.find((data) => data.DeviceUID === device.DeviceUID);
+
+    if (latestData) {
+      const { DeviceUID, Temperature, Humidity } = latestData;
+      const status = new Date(latestData.Timestamp) >= new Date(Date.now() - 5 * 60 * 1000) ? 'online' : 'offline';
+      const triggerValue = device.TriggerValue;
+
+      if (Temperature > triggerValue) {
+        insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimeStamp, 'heating']);
+      } else {
+        insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimeStamp, status]);
+      }
+    }
   });
+
+  if (insertLogValues.length > 0) {
+    db.query(insertLogQuery, [insertLogValues], (error) => {
+      if (error) {
+        console.error('Error inserting the device data into tms_log: ', error);
+        return;
+      }
+    });
+  }
+});
+  
 }
 
 
