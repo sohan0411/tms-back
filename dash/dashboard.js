9723f7274c111808aaa7b4f1e7608a1e9a954998
Kaushalpohekar <Kaushalpohekar1@gmail.com>
2023-07-18 16:58:46 +0530

    New

--- dash/dashboard.js
+++ dash/dashboard.js
@@ -687,7 +687,7 @@ function fetchCompanyUser(req, res) {
         throw new Error('Error fetching users');
       }
 
-      res.json(users);
+      res.status(200).json(users);
     });
   } catch (error) {
     console.error('Error fetching devices:', error);
