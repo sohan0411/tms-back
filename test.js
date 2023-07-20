3c2100623388c2a37a50e89749ec0b5b7452aeb9
Kaushalpohekar <Kaushalpohekar1@gmail.com>
2023-07-20 09:50:36 +0530

    Kaushal

--- test.js
+++ test.js
@@ -29,4 +29,30 @@ function testData() {
   }
 }
 
+function testData2() {
+  for (let i = 10; i <= 100; i++) {
+    const DeviceUID = `SL012023${i}`;
+    const Temperature = getRandomNumber(35, 50).toFixed(2);
+    const Humidity = getRandomNumber(40, 70).toFixed(2);
+    const TimeStamp = new Date().toISOString();
+
+    const data = {
+      DeviceUID,
+      Temperature,
+      Humidity,
+      TimeStamp
+    };
+
+    const entry = "INSERT INTO actual_data SET ?";
+    db.query(entry, data, (error, results) => {
+      if (error) {
+        console.log("Error while inserting data", error);
+      } else {
+        /*console.log("Data inserted successfully!");*/
+      }
+    });
+  }
+}
+
 setInterval(testData, 20000);
+setInterval(testData2, 20000);
