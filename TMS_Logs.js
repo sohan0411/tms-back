b3026f7cfbdf28abc0e274bdb79ff99cb310df0b
Soumya2022 <soumyalokhande28@gmil.com>
2023-09-06 17:08:16 +0530

    MQTT

--- TMS_Logs.js
+++ TMS_Logs.js
@@ -1,3 +1,82 @@
+// const db = require('./db');
+
+// function monitorDevice() {
+//   const selectTriggerQuery = 'SELECT tms_devices.DeviceUID, tms_trigger.TriggerValue FROM tms_trigger JOIN tms_devices ON tms_trigger.DeviceUID = tms_devices.DeviceUID';
+
+//   db.query(selectTriggerQuery, (error, triggerResults) => {
+//     if (error) {
+//       console.error('Error executing the select query: ', error);
+//       return;
+//     }
+
+//     const deviceData = triggerResults.map((trigger) => ({
+//       DeviceUID: trigger.DeviceUID,
+//       TriggerValue: trigger.TriggerValue,
+//     }));
+
+//     const deviceUIDs = deviceData.map((device) => device.DeviceUID);
+
+//     const selectLatestDataQuery = `
+//       SELECT *
+//       FROM actual_data
+//       WHERE (DeviceUID, TimeStamp) IN (
+//         SELECT DeviceUID, MAX(TimeStamp) AS MaxTimeStamp
+//         FROM actual_data
+//         WHERE DeviceUID IN (${deviceUIDs.map(() => '?').join(', ')})
+//         GROUP BY DeviceUID
+//       )`;
+
+//     db.query(selectLatestDataQuery, deviceUIDs, (error, latestDataResults) => {
+//       if (error) {
+//         console.error('Error executing the latest data select query: ', error);
+//         return;
+//       }
+
+//       const insertLogQuery = 'INSERT INTO tms_trigger_logs (DeviceUID, Temperature, Humidity, TimeStamp, Status,ip_address) VALUES ?';
+//       const insertLogValues = [];
+//       const currentTimestamp = new Date().toISOString();
+
+//       deviceData.forEach((device) => {
+//         const latestData = latestDataResults.find((data) => data.DeviceUID === device.DeviceUID);
+
+//         if (latestData) {
+//           const { DeviceUID, Temperature, Humidity, TimeStamp } = latestData;
+//           const latestDateTime = new Date(TimeStamp);
+          
+//           const timeDifference = new Date() - latestDateTime;
+          
+//           const isDeviceOnline = timeDifference <= 5 * 60 * 1000;
+
+//           if (isDeviceOnline) {
+//             if (Temperature > device.TriggerValue) {
+//               insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'heating']);
+//               //console.log(`DeviceUID: ${DeviceUID}, Status: heating`);
+//             } else {
+//               insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'online']);
+//               //console.log(`DeviceUID: ${DeviceUID}, Status: online`);
+//             }
+//           } else {
+//             insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'offline']);
+//             //console.log(`DeviceUID: ${DeviceUID}, Status: offline`);
+//           }
+//         }
+//       });
+
+//       if (insertLogValues.length > 0) {
+//         db.query(insertLogQuery, [insertLogValues], (error) => {
+//           if (error) {
+//             console.error('Error inserting the device data into tms_log: ', error);
+//             return;
+//           }
+//         });
+//       }
+//     });
+//   });
+// }
+
+// setInterval(monitorDevice, 20000);
+
+
 const db = require('./db');
 
 function monitorDevice() {
@@ -17,14 +96,17 @@ function monitorDevice() {
     const deviceUIDs = deviceData.map((device) => device.DeviceUID);
 
     const selectLatestDataQuery = `
-      SELECT *
-      FROM actual_data
-      WHERE (DeviceUID, TimeStamp) IN (
-        SELECT DeviceUID, MAX(TimeStamp) AS MaxTimeStamp
-        FROM actual_data
-        WHERE DeviceUID IN (${deviceUIDs.map(() => '?').join(', ')})
-        GROUP BY DeviceUID
-      )`;
+  SELECT ad.*, tms_devices.ip_address
+  FROM actual_data ad
+  JOIN (
+    SELECT DeviceUID, MAX(TimeStamp) AS MaxTimeStamp
+    FROM actual_data
+    WHERE DeviceUID IN (${deviceUIDs.map(() => '?').join(', ')})
+    GROUP BY DeviceUID
+  ) AS td
+  ON ad.DeviceUID = td.DeviceUID AND ad.TimeStamp = td.MaxTimeStamp
+  JOIN tms_devices ON ad.DeviceUID = tms_devices.DeviceUID
+`;
 
     db.query(selectLatestDataQuery, deviceUIDs, (error, latestDataResults) => {
       if (error) {
@@ -32,7 +114,7 @@ function monitorDevice() {
         return;
       }
 
-      const insertLogQuery = 'INSERT INTO tms_trigger_logs (DeviceUID, Temperature, Humidity, TimeStamp, Status) VALUES ?';
+      const insertLogQuery = 'INSERT INTO tms_trigger_logs (DeviceUID, Temperature, Humidity, TimeStamp, Status, ip_address) VALUES ?';
       const insertLogValues = [];
       const currentTimestamp = new Date().toISOString();
 
@@ -40,24 +122,17 @@ function monitorDevice() {
         const latestData = latestDataResults.find((data) => data.DeviceUID === device.DeviceUID);
 
         if (latestData) {
-          const { DeviceUID, Temperature, Humidity, TimeStamp } = latestData;
+          const { DeviceUID, Temperature, Humidity, TimeStamp, ip_address } = latestData;
           const latestDateTime = new Date(TimeStamp);
-          
+
           const timeDifference = new Date() - latestDateTime;
-          
+
           const isDeviceOnline = timeDifference <= 5 * 60 * 1000;
 
           if (isDeviceOnline) {
-            if (Temperature > device.TriggerValue) {
-              insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'heating']);
-              //console.log(`DeviceUID: ${DeviceUID}, Status: heating`);
-            } else {
-              insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'online']);
-              //console.log(`DeviceUID: ${DeviceUID}, Status: online`);
-            }
+            insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'heating', ip_address]);
           } else {
-            insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'offline']);
-            //console.log(`DeviceUID: ${DeviceUID}, Status: offline`);
+            insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'offline', ip_address]);
           }
         }
       });
---
+++ sub.js
@@ -0,0 +1,93 @@
+const mqtt = require('mqtt');
+const mysql = require('mysql2');
+const os = require('os');
+
+// MQTT broker URL
+const broker = 'ws://broker.emqx.io:8083/mqtt';
+
+// MySQL configuration
+const mysqlConfig = {
+    host: 'senselivedb.cn5vfllmzwrp.ap-south-1.rds.amazonaws.com',
+        user: 'admin',
+        password: 'sense123',
+        database: 'tmp',
+  port: 3306, // MySQL default port is 3306
+};
+
+// Create a MySQL connection pool
+const mysqlPool = mysql.createPool(mysqlConfig);
+
+// Fetch the local IP address
+const localIpAddress = getLocalIpAddress();
+
+function getLocalIpAddress() {
+  const interfaces = os.networkInterfaces();
+  for (const key in interfaces) {
+    const iface = interfaces[key];
+    for (const item of iface) {
+      if (item.family === 'IPv4' && !item.internal) {
+        return item.address;
+      }
+    }
+  }
+  return 'Unknown'; // Return 'Unknown' if no IP address is found
+}
+
+console.log('Local IP Address:', localIpAddress);
+
+// Connect to the MQTT broker
+const mqttClient = mqtt.connect(broker);
+
+// Handle MQTT connection event
+mqttClient.on('connect', () => {
+  console.log('Connected to MQTT broker');
+
+  for (let i = 1; i <= 9; i++) {
+    const deviceId = `SL0120230${i}`;
+    const topic = `sense/live/${deviceId}`;
+    mqttClient.subscribe(topic, (error) => {
+      if (error) {
+        console.error(`Error subscribing to ${topic}:`, error);
+      } else {
+        console.log(`Subscribed to ${topic}`);
+      }
+    });
+  }
+});
+
+mqttClient.on('message', (topic, message) => {
+  try {
+    const data = JSON.parse(message);
+
+    const insertQuery = `
+    INSERT INTO actual_data (DeviceUID, Temperature, Humidity, Timestamp,ip_address)
+    VALUES (?, ?, ?, ?,?)
+    `;
+
+    const insertValues = [
+      data.DeviceUID,
+      data.Temperature,
+      data.Humidity,
+      data.Timestamp,
+      localIpAddress,
+    ];
+
+    mysqlPool.query(insertQuery, insertValues, (error) => {
+      if (error) {
+        console.error('Error inserting data into MySQL:', error);
+      } else {
+        console.log('Data inserted into MySQL');
+      }
+    });
+  } catch (error) {
+    console.error('Error processing message:', error);
+  }
+});
+
+mqttClient.on('error', (error) => {
+  console.error('MQTT error:', error);
+});
+
+process.on('exit', () => {
+  mysqlPool.end();
+});
