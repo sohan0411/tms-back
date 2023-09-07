a869f1af63b6fc415b366d131cfc17f8a84950c0
Soumya2022 <soumyalokhande28@gmil.com>
2023-09-07 15:34:56 +0530

    status

--- TMS_Logs.js
+++ TMS_Logs.js
@@ -1,76 +1,3 @@
-const db = require('./db');
-
-function monitorDevice() {
-  const selectTriggerQuery = 'SELECT tms_devices.DeviceUID, tms_trigger.TriggerValue FROM tms_trigger JOIN tms_devices ON tms_trigger.DeviceUID = tms_devices.DeviceUID';
-
-  db.query(selectTriggerQuery, (error, triggerResults) => {
-    if (error) {
-      console.error('Error executing the select query: ', error);
-      return;
-    }
-
-    const deviceData = triggerResults.map((trigger) => ({
-      DeviceUID: trigger.DeviceUID,
-      TriggerValue: trigger.TriggerValue,
-    }));
-
-    const deviceUIDs = deviceData.map((device) => device.DeviceUID);
-
-    const selectLatestDataQuery = `
-      SELECT ad.DeviceUID, ad.Temperature, ad.Humidity, ad.TimeStamp, ad.ip_address, MAX(ad.TimeStamp) AS MaxTimeStamp
-      FROM actual_data ad
-      WHERE ad.DeviceUID IN (${deviceUIDs.map(() => '?').join(', ')})
-      GROUP BY ad.DeviceUID
-    `;
-
-    db.query(selectLatestDataQuery, deviceUIDs, (error, latestDataResults) => {
-      if (error) {
-        console.error('Error executing the latest data select query: ', error);
-        return;
-      }
-
-      const insertLogQuery = 'INSERT INTO tms_trigger_logs (DeviceUID, Temperature, Humidity, TimeStamp, Status, ip_address) VALUES ?';
-      const insertLogValues = [];
-      const currentTimestamp = new Date().toISOString();
-
-      deviceData.forEach((device) => {
-        const latestData = latestDataResults.find((data) => data.DeviceUID === device.DeviceUID);
-
-        if (latestData) {
-          const { DeviceUID, Temperature, Humidity, TimeStamp, ip_address } = latestData;
-          const latestDateTime = new Date(TimeStamp);
-
-          const timeDifference = new Date() - latestDateTime;
-
-          const isDeviceOnline = timeDifference <= 5 * 60 * 1000;
-
-          if (isDeviceOnline) {
-            if (Temperature > device.TriggerValue) {
-              insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'heating', ip_address]);
-            } else {
-              insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'online', ip_address]);
-            }
-          } else {
-            insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'offline', ip_address]);
-          }
-        }
-      });
-
-      if (insertLogValues.length > 0) {
-        db.query(insertLogQuery, [insertLogValues], (error) => {
-          if (error) {
-            console.error('Error inserting the device data into tms_log: ', error);
-            return;
-          }
-        });
-      }
-    });
-  });
-}
-
-setInterval(monitorDevice, 20000);
-
-
 // const db = require('./db');
 
 // function monitorDevice() {
@@ -90,14 +17,11 @@ setInterval(monitorDevice, 20000);
 //     const deviceUIDs = deviceData.map((device) => device.DeviceUID);
 
 //     const selectLatestDataQuery = `
-//       SELECT *
-//       FROM actual_data
-//       WHERE (DeviceUID, TimeStamp) IN (
-//         SELECT DeviceUID, MAX(TimeStamp) AS MaxTimeStamp
-//         FROM actual_data
-//         WHERE DeviceUID IN (${deviceUIDs.map(() => '?').join(', ')})
-//         GROUP BY DeviceUID
-//       )`;
+//       SELECT ad.DeviceUID, ad.Temperature, ad.Humidity, ad.TimeStamp, ad.ip_address, MAX(ad.TimeStamp) AS MaxTimeStamp
+//       FROM actual_data ad
+//       WHERE ad.DeviceUID IN (${deviceUIDs.map(() => '?').join(', ')})
+//       GROUP BY ad.DeviceUID
+//     `;
 
 //     db.query(selectLatestDataQuery, deviceUIDs, (error, latestDataResults) => {
 //       if (error) {
@@ -105,7 +29,7 @@ setInterval(monitorDevice, 20000);
 //         return;
 //       }
 
-//       const insertLogQuery = 'INSERT INTO tms_trigger_logs (DeviceUID, Temperature, Humidity, TimeStamp, Status,ip_address) VALUES ?';
+//       const insertLogQuery = 'INSERT INTO tms_trigger_logs (DeviceUID, Temperature, Humidity, TimeStamp, Status, ip_address) VALUES ?';
 //       const insertLogValues = [];
 //       const currentTimestamp = new Date().toISOString();
 
@@ -113,24 +37,21 @@ setInterval(monitorDevice, 20000);
 //         const latestData = latestDataResults.find((data) => data.DeviceUID === device.DeviceUID);
 
 //         if (latestData) {
-//           const { DeviceUID, Temperature, Humidity, TimeStamp } = latestData;
+//           const { DeviceUID, Temperature, Humidity, TimeStamp, ip_address } = latestData;
 //           const latestDateTime = new Date(TimeStamp);
-          
+
 //           const timeDifference = new Date() - latestDateTime;
-          
+
 //           const isDeviceOnline = timeDifference <= 5 * 60 * 1000;
 
 //           if (isDeviceOnline) {
 //             if (Temperature > device.TriggerValue) {
-//               insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'heating']);
-//               //console.log(`DeviceUID: ${DeviceUID}, Status: heating`);
+//               insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'heating', ip_address]);
 //             } else {
-//               insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'online']);
-//               //console.log(`DeviceUID: ${DeviceUID}, Status: online`);
+//               insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'online', ip_address]);
 //             }
 //           } else {
-//             insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'offline']);
-//             //console.log(`DeviceUID: ${DeviceUID}, Status: offline`);
+//             insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'offline', ip_address]);
 //           }
 //         }
 //       });
@@ -148,3 +69,82 @@ setInterval(monitorDevice, 20000);
 // }
 
 // setInterval(monitorDevice, 20000);
+
+
+const db = require('./db');
+
+function monitorDevice() {
+  const selectTriggerQuery = 'SELECT tms_devices.DeviceUID, tms_trigger.TriggerValue FROM tms_trigger JOIN tms_devices ON tms_trigger.DeviceUID = tms_devices.DeviceUID';
+
+  db.query(selectTriggerQuery, (error, triggerResults) => {
+    if (error) {
+      console.error('Error executing the select query: ', error);
+      return;
+    }
+
+    const deviceData = triggerResults.map((trigger) => ({
+      DeviceUID: trigger.DeviceUID,
+      TriggerValue: trigger.TriggerValue,
+    }));
+
+    const deviceUIDs = deviceData.map((device) => device.DeviceUID);
+
+    const selectLatestDataQuery = `
+      SELECT *
+      FROM actual_data
+      WHERE (DeviceUID, TimeStamp) IN (
+        SELECT DeviceUID, MAX(TimeStamp) AS MaxTimeStamp
+        FROM actual_data
+        WHERE DeviceUID IN (${deviceUIDs.map(() => '?').join(', ')})
+        GROUP BY DeviceUID
+      )`;
+
+    db.query(selectLatestDataQuery, deviceUIDs, (error, latestDataResults) => {
+      if (error) {
+        console.error('Error executing the latest data select query: ', error);
+        return;
+      }
+
+      const insertLogQuery = 'INSERT INTO tms_trigger_logs (DeviceUID, Temperature, Humidity, TimeStamp, Status) VALUES ?';
+      const insertLogValues = [];
+      const currentTimestamp = new Date().toISOString();
+
+      deviceData.forEach((device) => {
+        const latestData = latestDataResults.find((data) => data.DeviceUID === device.DeviceUID);
+
+        if (latestData) {
+          const { DeviceUID, Temperature, Humidity, TimeStamp } = latestData;
+          const latestDateTime = new Date(TimeStamp);
+          
+          const timeDifference = new Date() - latestDateTime;
+          
+          const isDeviceOnline = timeDifference <= 5 * 60 * 1000;
+
+          if (isDeviceOnline) {
+            if (Temperature > device.TriggerValue) {
+              insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'heating']);
+              //console.log(`DeviceUID: ${DeviceUID}, Status: heating`);
+            } else {
+              insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'online']);
+              //console.log(`DeviceUID: ${DeviceUID}, Status: online`);
+            }
+          } else {
+            insertLogValues.push([DeviceUID, Temperature, Humidity, currentTimestamp, 'offline']);
+            //console.log(`DeviceUID: ${DeviceUID}, Status: offline`);
+          }
+        }
+      });
+
+      if (insertLogValues.length > 0) {
+        db.query(insertLogQuery, [insertLogValues], (error) => {
+          if (error) {
+            console.error('Error inserting the device data into tms_log: ', error);
+            return;
+          }
+        });
+      }
+    });
+  });
+}
+
+setInterval(monitorDevice, 20000);
--- devices_api.js
+++ devices_api.js
@@ -43,3 +43,54 @@ function runCode() {
 }
 
 runCode();
+
+// const db = require('./db');
+
+// function fetchLatestDeviceData(limit, callback) {
+//   const selectQuery = `
+//     SELECT
+//       a.deviceuid,
+//       a.ip_address,
+//       a.timestamp,
+//       t.Status
+//     FROM
+//       actual_data a
+//     LEFT JOIN
+//       tms_trigger_logs t ON a.deviceuid = t.deviceuid
+//     ORDER BY
+//       a.timestamp DESC
+//     LIMIT ?
+//   `;
+
+//   console.log('Executing SQL Query:', selectQuery); // Add this line to log the query execution
+
+//   db.query(selectQuery, [limit], (error, results) => {
+//     if (error) {
+//       console.error('Error fetching latest device data:', error);
+//       callback(error, null);
+//     } else {
+//       const devices = results;
+//       callback(null, devices);
+//     }
+//   });
+// }
+
+// const limit = 9;
+
+// function runCode() {
+//   fetchLatestDeviceData(limit, (error, devices) => {
+//     if (error) {
+//       console.error('Error:', error);
+//     } else {
+//       console.log('Latest Device Data:');
+//       devices.forEach((device) => {
+//         console.log(
+//           `Device ID: ${device.deviceuid}, IP Address: ${device.ip_address}, Timestamp: ${device.timestamp}, Status: ${device.status}`
+//         );
+//       });
+//     }
+//     setTimeout(runCode, 5000);
+//   });
+// }
+
+// runCode();
