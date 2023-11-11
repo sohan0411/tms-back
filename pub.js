9721a804aa742a25a164792a1ab6c7283df6cee6
Gaurav Jadhav <gauravjadhav173@gmail.com>
2023-11-11 14:42:30 +0530

    interval changes

--- pub.js
+++ pub.js
@@ -1,5 +1,4 @@
 const mqtt = require('mqtt');
-const moment = require('moment-timezone');
 
 // MQTT broker URL
 const brokerUrl = 'ws://broker.emqx.io:8083/mqtt';
@@ -8,12 +7,11 @@ function getRandomNumber(min, max) {
   return Math.random() * (max - min) + min;
 }
 
-// Function to generate random JSON data for a specific device ID
 function generateRandomData(deviceId) {
   const DeviceUID = deviceId;
   const Temperature = getRandomNumber(35, 50).toFixed(1);
   const Humidity = getRandomNumber(40, 70).toFixed(1);
-  const Timestamp = moment().tz('Asia/Kolkata').format('YYYY-MM-DDTHH:mm:ss');
+  const Timestamp = new Date().toISOString();
 
   const data = {
     DeviceUID,
@@ -24,15 +22,11 @@ function generateRandomData(deviceId) {
 
   return JSON.stringify(data);
 }
-
-// Connect to the MQTT broker
 const client = mqtt.connect(brokerUrl);
 
-// Handle MQTT connection event
 client.on('connect', () => {
-  //console.log('Connected to MQTT broker');
+  console.log('Connected to MQTT broker');
 
-  // Publish random data for each device ID every 1 second
   for (let i = 1; i <= 9; i++) {
     const deviceId = `SL0120230${i}`;
     const topic = `sense/live/${deviceId}`;
@@ -40,21 +34,17 @@ client.on('connect', () => {
     setInterval(() => {
       const message = generateRandomData(deviceId);
       client.publish(topic, message);
-    }, 20000);
+    }, 1000);
   }
-  // for (let i = 10; i <= 1000; i++) {
-  //     const deviceId = `SL012023${i}`;
-  //     const topic = `sense/tms/${deviceId}`;
+});
 
-  //     setInterval(() => {
-  //       const message = generateRandomData(deviceId);
-  //       client.publish(topic, message);
-  //     }, 20000);
-  //   }
-  }
-);
-
-// Handle MQTT error event
 client.on('error', (error) => {
   console.error('MQTT error:', error);
+});
+
+// Graceful shutdown
+process.on('SIGINT', () => {
+  console.log('Received SIGINT. Closing MQTT connection.');
+  client.end();
+  process.exit();
 });
