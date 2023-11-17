f8304064d564655a4478c7d98436ca8f5b2daa8c
Gaurav Jadhav <gauravjadhav173@gmail.com>
2023-11-17 17:17:32 +0530

    add dev

--- dash/dashboard.js
+++ dash/dashboard.js
@@ -746,34 +746,35 @@ function addDeviceTrigger(req, res) {
 //   }
 // }
 function addDevice(req, res) {
-  const { DeviceUID, DeviceLocation, DeviceName, CompanyEmail, CompanyName, SMS, email, type } = req.body;
- try {
-   const checkDeviceQuery = 'SELECT * FROM tms_devices WHERE DeviceUID = ?';
-   const insertDeviceQuery = 'INSERT INTO tms_devices (DeviceUID, DeviceLocation, DeviceName, CompanyEmail, CompanyName, IssueDate, SMS, email, type, endDate) VALUES (?, ?, ?, ?, ?, CURRENT_TIMESTAMP, ?, ?, ?, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 365 DAY))';
+  const { DeviceUID, DeviceLocation, DeviceName, CompanyEmail, CompanyName, SMS, email, type, DeviceType } = req.body;
 
-   db.query(checkDeviceQuery, [DeviceUID], (error, checkResult) => {
-     if (error) {
-       console.error('Error while checking device:', error);
-       return res.status(500).json({ message: 'Internal server error' });
-     }
+  try {
+    const checkDeviceQuery = 'SELECT * FROM tms_devices WHERE DeviceUID = ?';
+    const insertDeviceQuery = 'INSERT INTO tms_devices (DeviceUID, DeviceLocation, DeviceName, CompanyEmail, CompanyName, IssueDate, SMS, email, type, DeviceType, endDate) VALUES (?, ?, ?, ?, ?, CURRENT_TIMESTAMP, ?, ?, ?, ?, DATE_ADD(CURRENT_TIMESTAMP, INTERVAL 365 DAY))';
 
-     if (checkResult.length > 0) {
-       return res.status(400).json({ message: 'Device already added' });
-     }
+    db.query(checkDeviceQuery, [DeviceUID], (error, checkResult) => {
+      if (error) {
+        console.error('Error while checking device:', error);
+        return res.status(500).json({ message: 'Internal server error' });
+      }
 
-     db.query(insertDeviceQuery, [DeviceUID,DeviceLocation,DeviceName,CompanyEmail,CompanyName,SMS,email,type], (insertError, insertResult) => {
-       if (insertError) {
-         console.error('Error while inserting device:', insertError);
-         return res.status(500).json({ message: 'Internal server error' });
-       }
+      if (checkResult.length > 0) {
+        return res.status(400).json({ message: 'Device already added' });
+      }
 
-       return res.json({ message: 'Device added successfully!' });
-     });
-   });
- } catch (error) {
-   console.error('Error in device check:', error);
-   res.status(500).json({ message: 'Internal server error' });
- }
+      db.query(insertDeviceQuery, [DeviceUID, DeviceLocation, DeviceName, CompanyEmail, CompanyName, SMS, email, type, DeviceType], (insertError, insertResult) => {
+        if (insertError) {
+          console.error('Error while inserting device:', insertError);
+          return res.status(500).json({ message: 'Internal server error' });
+        }
+
+        return res.json({ message: 'Device added successfully!' });
+      });
+    });
+  } catch (error) {
+    console.error('Error in device check:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 }
 
 
